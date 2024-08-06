720c02d8d2a04028beb0c2e1edb39518ff74bea8
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-08-06 16:33:10 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,10 +1,10 @@
-
-a = 57
-b = 7
-print(a+b)
-print(a-b)
-print(a*b)
-print((a+b)/2)
-print(a//b)
-print(a%b)
-print(a/b)
+def math():
+    a = 57
+    b = 7
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print((a+b)/2)
+    print(a//b)
+    print(a%b)
+    print(a/b)
